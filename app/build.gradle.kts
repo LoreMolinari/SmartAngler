dfe642ac869b37558e1b42908bf0f5d76d5c093d
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-17 09:40:59 +0100

    Switch to JDK 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,3 +48,8 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
