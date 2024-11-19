8ffc1da285c1efa086985bc5b70c9663c154330a
LoreMolinari <lore.molinari.00@gmail.com>
2024-11-19 17:39:05 +0100

    Code refactor, fixed some warnings and better ui for home

--- app/src/main/java/com/smartangler/smartangler/ui/home/HomeFragment.java
+++ app/src/main/java/com/smartangler/smartangler/ui/home/HomeFragment.java
@@ -20,35 +20,26 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.smartangler.smartangler.SmartAnglerOpenHelper;
-import com.smartangler.smartangler.R;
-import com.smartangler.smartangler.databinding.FragmentHomeBinding;
 import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.smartangler.smartangler.R;
+import com.smartangler.smartangler.SmartAnglerOpenHelper;
+import com.smartangler.smartangler.databinding.FragmentHomeBinding;
 
 import java.text.SimpleDateFormat;
-import java.util.TimeZone;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
     private TextView stepCountsView;
     private CircularProgressIndicator progressBar;
-
     private MaterialButtonToggleGroup toggleButtonGroup;
-
     private Sensor accSensor;
-
     private SensorManager sensorManager;
-
     private StepCounterListener sensorListener;
-
-    private Sensor stepDetectorSensor;
-
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -59,43 +50,38 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        stepCountsView = (TextView) root.findViewById(R.id.counter);
+        stepCountsView = root.findViewById(R.id.counter);
         stepCountsView.setText("0");
 
-        progressBar = (CircularProgressIndicator) root.findViewById(R.id.progressBar);
+        progressBar = root.findViewById(R.id.progressBar);
         progressBar.setMax(50);
         progressBar.setProgress(0);
 
-        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        try {
+            sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        } catch (NullPointerException e) {
+            Toast.makeText(getContext(), R.string.acc_sensor_not_available, Toast.LENGTH_LONG).show();
+        }
         accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-
-        stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
         SmartAnglerOpenHelper databaseOpenHelper = new SmartAnglerOpenHelper(this.getContext());
         SQLiteDatabase database = databaseOpenHelper.getWritableDatabase();
 
 
-
-        toggleButtonGroup = (MaterialButtonToggleGroup) root.findViewById(R.id.toggleButtonGroup);
+        toggleButtonGroup = root.findViewById(R.id.toggleButtonGroup);
         toggleButtonGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
             @Override
             public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
-                if (group.getCheckedButtonId() ==R.id.start_button)
-                {
-                    if (accSensor != null)
-                    {
+                if (group.getCheckedButtonId() == R.id.start_button) {
+                    if (accSensor != null) {
                         sensorListener = new StepCounterListener(stepCountsView, progressBar, database);
                         sensorManager.registerListener(sensorListener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
                         Toast.makeText(getContext(), R.string.start_text, Toast.LENGTH_LONG).show();
-                    }
-                    else
-                    {
+                    } else {
                         Toast.makeText(getContext(), R.string.acc_sensor_not_available, Toast.LENGTH_LONG).show();
                     }
 
-                }
-                else
-                {
+                } else {
                     sensorManager.unregisterListener(sensorListener);
                     Toast.makeText(getContext(), R.string.stop_text, Toast.LENGTH_LONG).show();
                 }
@@ -112,12 +98,12 @@ public class HomeFragment extends Fragment {
     }
 }
 
-class  StepCounterListener implements SensorEventListener{
+class StepCounterListener implements SensorEventListener {
 
     private long lastSensorUpdate = 0;
     public static int accStepCounter = 0;
-    ArrayList<Integer> accSeries = new ArrayList<Integer>();
-    ArrayList<String> timestampsSeries = new ArrayList<String>();
+    ArrayList<Integer> accSeries = new ArrayList<>();
+    ArrayList<String> timestampsSeries = new ArrayList<>();
     private double accMag = 0;
     private int lastAddedIndex = 1;
     int stepThreshold = 6;
@@ -125,73 +111,64 @@ class  StepCounterListener implements SensorEventListener{
     TextView stepCountsView;
 
     CircularProgressIndicator progressBar;
-    private SQLiteDatabase database;
+    private final SQLiteDatabase database;
 
     private String timestamp;
     private String day;
     private String hour;
 
 
-    public StepCounterListener(TextView stepCountsView, CircularProgressIndicator progressBar,  SQLiteDatabase databse)
-    {
+    public StepCounterListener(TextView stepCountsView, CircularProgressIndicator progressBar, SQLiteDatabase database) {
         this.stepCountsView = stepCountsView;
-        this.database = databse;
+        this.database = database;
         this.progressBar = progressBar;
     }
 
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        switch (sensorEvent.sensor.getType())
-        {
-            case Sensor.TYPE_LINEAR_ACCELERATION:
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
 
-                float x = sensorEvent.values[0];
-                float y = sensorEvent.values[1];
-                float z = sensorEvent.values[2];
+            long currentTimeInMilliSecond = System.currentTimeMillis();
 
-                long currentTimeInMilliSecond = System.currentTimeMillis();
+            long timeInMillis = currentTimeInMilliSecond + (sensorEvent.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
 
-                long timeInMillis = currentTimeInMilliSecond + (sensorEvent.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
-
-                // Convert the timestamp to date
-                SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
-                jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
-                String sensorEventDate = jdf.format(timeInMillis);
+            // Convert the timestamp to date
+            SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+            jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
+            String sensorEventDate = jdf.format(timeInMillis);
 
 
+            if ((currentTimeInMilliSecond - lastSensorUpdate) > 1000) {
+                lastSensorUpdate = currentTimeInMilliSecond;
+                String sensorRawValues = "  x = " + x + "  y = " + y + "  z = " + z;
+                Log.d("Acc. Event", "last sensor update at " + sensorEventDate + sensorRawValues);
+            }
 
 
-                if ((currentTimeInMilliSecond - lastSensorUpdate) > 1000)
-                {
-                    lastSensorUpdate = currentTimeInMilliSecond;
-                    String sensorRawValues = "  x = "+ String.valueOf(x) +"  y = "+ String.valueOf(y) +"  z = "+ String.valueOf(z);
-                    Log.d("Acc. Event", "last sensor update at " + String.valueOf(sensorEventDate) + sensorRawValues);
-                }
+            accMag = Math.sqrt(x * x + y * y + z * z);
 
 
-                accMag = Math.sqrt(x*x+y*y+z*z);
+            accSeries.add((int) accMag);
+
+            // Get the date, the day and the hour
+            timestamp = sensorEventDate;
+            day = sensorEventDate.substring(0, 10);
+            hour = sensorEventDate.substring(11, 13);
+
+            Log.d("SensorEventTimestampInMilliSecond", timestamp);
 
 
-                accSeries.add((int) accMag);
-
-                // Get the date, the day and the hour
-                timestamp = sensorEventDate;
-                day = sensorEventDate.substring(0,10);
-                hour = sensorEventDate.substring(11,13);
-
-                Log.d("SensorEventTimestampInMilliSecond", timestamp);
-
-
-                timestampsSeries.add(timestamp);
-                peakDetection();
-
-                break;
-
+            timestampsSeries.add(timestamp);
+            peakDetection();
         }
 
 
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
@@ -207,11 +184,11 @@ class  StepCounterListener implements SensorEventListener{
             return;
         }
 
-        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex,currentSize);
-        List<String> timePointList = timestampsSeries.subList(lastAddedIndex,currentSize);
+        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex, currentSize);
+        List<String> timePointList = timestampsSeries.subList(lastAddedIndex, currentSize);
         lastAddedIndex = currentSize;
 
-        for (int i = 1; i < valuesInWindow.size()-1; i++) {
+        for (int i = 1; i < valuesInWindow.size() - 1; i++) {
             int forwardSlope = valuesInWindow.get(i + 1) - valuesInWindow.get(i);
             int downwardSlope = valuesInWindow.get(i) - valuesInWindow.get(i - 1);
 
--- app/src/main/java/com/smartangler/smartangler/ui/statistics/StatisticsViewModel.java
+++ app/src/main/java/com/smartangler/smartangler/ui/statistics/StatisticsViewModel.java
@@ -10,7 +10,7 @@ public class StatisticsViewModel extends ViewModel {
 
     public StatisticsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("Loading");
     }
 
     public LiveData<String> getText() {
