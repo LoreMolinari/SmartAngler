3efb98a2844dc46d30fe9f62c54832140a376633
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-23 15:36:41 +0100

    Implemented basic cards to view fish DB data (#6)
    * Added card view
    
    * Move card to its own file
    
    * Syntax
    
    * Using recyclerView
    
    * Removed old TextView
    
    * Fix cards spacing
    
    * Added fish card pictures
    
    * Fetching all fish data from DB
    
    * Removed default text
    
    * Remove placeholder
    
    * Visualizing all data
    
    * Add picture description
    
    * Added placeholder fish image
    
    * Remove unused items

--- app/src/main/java/com/smartangler/smartangler/ui/fishDB/FishDBFragment.java
+++ app/src/main/java/com/smartangler/smartangler/ui/fishDB/FishDBFragment.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.smartangler.smartangler.Fish;
@@ -27,7 +30,6 @@ public class FishDBFragment extends Fragment {
     private FishDBViewModel mViewModel;
     private FragmentFishDbBinding binding;
     private List<Fish> fishList;
-    private TextView textFishDB;
 
     public static FishDBFragment newInstance() {
         return new FishDBFragment();
@@ -39,6 +41,9 @@ public class FishDBFragment extends Fragment {
         binding = FragmentFishDbBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         SmartAnglerOpenHelper databaseOpenHelper = new SmartAnglerOpenHelper(this.getContext());
 
         fishList = new ArrayList<>();
@@ -49,8 +54,8 @@ public class FishDBFragment extends Fragment {
             fishString = fishString.concat(fish.toString());
         }
 
-        textFishDB = (TextView) root.findViewById(R.id.text_fishDB);
-        textFishDB.setText(fishString);
+        ItemAdapter adapter = new ItemAdapter(fishList);
+        recyclerView.setAdapter(adapter);
 
         return root;
     }
@@ -62,4 +67,93 @@ public class FishDBFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+}
+
+class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+    private List<Fish> fishList;
+
+    public ItemAdapter(List<Fish> fishList) {
+        this.fishList = fishList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item_fish_db, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fish item = fishList.get(position);
+        if (item.getName().equals("Perch")) {
+            holder.fishPictureImageView.setImageResource(R.drawable.perch);
+            holder.fishPictureImageView.setContentDescription("A picture of a perch");
+        } else if (item.getName().equals("Zander")) {
+            holder.fishPictureImageView.setImageResource(R.drawable.zander);
+            holder.fishPictureImageView.setContentDescription("A picture of a zander");
+        } else {
+            holder.fishPictureImageView.setImageResource(R.drawable.generic_fish);
+        }
+        holder.fishNameTextView.setText(item.getName());
+        holder.fishDescriptionTextView.setText(item.getDescription());
+
+        String techniquesText = "Suggested techniques: ";
+        techniquesText = techniquesText.concat(String.join(", ", item.getTechniques()));
+        holder.fishTechniquesTextView.setText(techniquesText);
+
+        String baitsAndLuresText = "Suggested baits and lures: ";
+        baitsAndLuresText = baitsAndLuresText.concat(String.join(", ", item.getBaitsAndLures()));
+        holder.fishBaitsAndLuresTextView.setText(baitsAndLuresText);
+
+        // I don't love this at all
+        String seasonsText = "Active seasons: ";
+        List<String> seasonList = new ArrayList<>();
+        for (Fish.Season season : item.getSeasons()) {
+            String seasonString = season.toString();
+            seasonString = seasonString.substring(0, 1).toUpperCase() +
+                    seasonString.substring(1).toLowerCase();
+            seasonList.add(seasonString);
+        }
+        seasonsText = seasonsText.concat(String.join(", ", seasonList));
+        holder.fishSeasonsTextView.setText(seasonsText);
+
+        String timesOfDayText = "Active times of day: ";
+        List<String> timesOfDayList = new ArrayList<>();
+        for (Fish.TimeOfDay timeOfDay : item.getTimesOfDay()) {
+            String timeOfDayString = timeOfDay.toString();
+            timeOfDayString = timeOfDayString.substring(0, 1).toUpperCase() +
+                    timeOfDayString.substring(1).toLowerCase();
+            timesOfDayList.add(timeOfDayString);
+        }
+        timesOfDayText = timesOfDayText.concat(String.join(", ", timesOfDayList));
+        holder.fishTimesOfDayTextView.setText(timesOfDayText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fishList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView fishPictureImageView;
+        TextView fishNameTextView;
+        TextView fishDescriptionTextView;
+        TextView fishTechniquesTextView;
+        TextView fishBaitsAndLuresTextView;
+        TextView fishSeasonsTextView;
+        TextView fishTimesOfDayTextView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            fishPictureImageView = itemView.findViewById(R.id.picture_fish);
+            fishNameTextView = itemView.findViewById(R.id.text_fish_name);
+            fishDescriptionTextView = itemView.findViewById(R.id.text_fish_description);
+            fishTechniquesTextView = itemView.findViewById(R.id.text_techniques);
+            fishBaitsAndLuresTextView = itemView.findViewById(R.id.text_baits_and_lures);
+            fishSeasonsTextView = itemView.findViewById(R.id.text_seasons);
+            fishTimesOfDayTextView = itemView.findViewById(R.id.text_times_of_day);
+        }
+    }
 }
