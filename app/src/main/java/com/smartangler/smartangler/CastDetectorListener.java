fa6c19e969b481b05fc09e82d8bc0e36153542b5
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-25 11:58:41 +0100

    Use dedicated listener

--- app/src/main/java/com/smartangler/smartangler/CastDetectorListener.java
+++ app/src/main/java/com/smartangler/smartangler/CastDetectorListener.java
@@ -3,11 +3,55 @@ package com.smartangler.smartangler;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 
 public class CastDetectorListener implements SensorEventListener {
-    @Override
-    public void onSensorChanged(SensorEvent event) {
+    private static final int CAST_THRESHOLD = 10;
 
+    private TextView castsView;
+    public static int castsCounter = 0;
+    private ArrayList<Integer> accSeries = new ArrayList<>();
+    private int lastAddedIndex = 1;
+    public CastDetectorListener(TextView castsView) {
+        this.castsView = castsView;
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        double accMag = Math.sqrt(x * x + y * y + z * z);
+
+        accSeries.add((int) accMag);
+
+        int windowSize = 20;
+        int currentSize = accSeries.size();
+        if (currentSize - lastAddedIndex < windowSize) {
+            return;
+        }
+
+        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex, currentSize);
+        lastAddedIndex = currentSize;
+
+        for (int i = 1; i < valuesInWindow.size() - 1; i++) {
+            int forwardSlope = valuesInWindow.get(i + 1) - valuesInWindow.get(i);
+            int downwardSlope = valuesInWindow.get(i) - valuesInWindow.get(i - 1);
+
+            // Peak due to cast
+            if (forwardSlope < 0 && downwardSlope > 0 && valuesInWindow.get(i) > CAST_THRESHOLD) {
+                castsCounter += 1;
+                castsView.setText("Casts: " + String.valueOf(castsCounter));
+                Log.d("Cast detection", "Cast detected");
+            }
+        }
     }
 
     @Override
