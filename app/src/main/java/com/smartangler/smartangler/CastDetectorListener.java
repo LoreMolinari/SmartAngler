6f431d69886e21854bb7592c813562092dfa8b2d
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-25 11:33:17 +0100

    Using dedicated listener class

---
+++ app/src/main/java/com/smartangler/smartangler/CastDetectorListener.java
@@ -0,0 +1,17 @@
+package com.smartangler.smartangler;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class CastDetectorListener implements SensorEventListener {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
