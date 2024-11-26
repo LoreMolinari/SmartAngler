1272373bde64880d56f70f124ef628926b15c7e9
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-26 17:09:37 +0100

    Implement cast detection in fishing session (#9)
    * Moved listener to own file
    
    * Switch on sensor type
    
    * Added countStep function
    
    * Using system step detector
    
    * Using step detector
    
    * Detect cast peak
    
    * Draft of cast detection
    
    * Database now stores casts per session
    
    * View casts on tag
    
    * Now visualizing cast count in session fragment
    
    * Now storing casts in DB
    
    * Remove stale TODO
    
    * Removed stale TODO
    
    * Using dedicated listener class
    
    * Use dedicated listener
    
    * Remove cast detection in step listener
    
    * Remove unused vars
    
    * Cleanup code
    
    * Add comment
    
    * Moved listener to own file
    
    * Switch on sensor type
    
    * Added countStep function
    
    * Using system step detector
    
    * Using step detector
    
    * Detect cast peak
    
    * Draft of cast detection
    
    * Database now stores casts per session
    
    * View casts on tag
    
    * Now visualizing cast count in session fragment
    
    * Now storing casts in DB
    
    * Remove stale TODO
    
    * Removed stale TODO
    
    * Using dedicated listener class
    
    * Use dedicated listener
    
    * Remove cast detection in step listener
    
    * Remove unused vars
    
    * Cleanup code
    
    * Add comment
    
    ---------
    
    Co-authored-by: LoreMolinari <lore.molinari.00@gmail.com>

---
+++ app/src/main/java/com/smartangler/smartangler/CastDetectorListener.java
@@ -0,0 +1,59 @@
+package com.smartangler.smartangler;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CastDetectorListener implements SensorEventListener {
+    private static final int CAST_THRESHOLD = 10;
+
+    private final TextView castsView;
+    public static int castsCounter = 0;
+    private final ArrayList<Integer> accSeries = new ArrayList<>();
+    private int lastAddedIndex = 1;
+    public CastDetectorListener(TextView castsView) {
+        this.castsView = castsView;
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        double accMag = Math.sqrt(x * x + y * y + z * z);
+
+        accSeries.add((int) accMag);
+
+        int windowSize = 20;
+        int currentSize = accSeries.size();
+        if (currentSize - lastAddedIndex < windowSize) {
+            return;
+        }
+
+        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex, currentSize);
+        lastAddedIndex = currentSize;
+
+        for (int i = 1; i < valuesInWindow.size() - 1; i++) {
+            int forwardSlope = valuesInWindow.get(i + 1) - valuesInWindow.get(i);
+            int downwardSlope = valuesInWindow.get(i) - valuesInWindow.get(i - 1);
+
+            // Peak due to cast
+            if (forwardSlope < 0 && downwardSlope > 0 && valuesInWindow.get(i) > CAST_THRESHOLD) {
+                castsCounter += 1;
+                String castsText = "Casts: " + castsCounter;
+                castsView.setText(castsText);
+                Log.d("Cast detection", "Cast detected");
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
