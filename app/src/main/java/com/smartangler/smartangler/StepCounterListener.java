e97f2dc7ec7eae9eb7f946c2da837d9400b46376
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-26 17:09:37 +0100

    Implement cast detection in fishing session (#9)
    * Moved listener to own file
    
    * Switch on sensor type
    
    * Added countStep function
    
    * Using system step detector
    
    * Using step detector
    
    * Detect cast peak
    
    * Draft of cast detection
    
    * Database now stores casts per session
    
    * View casts on tag
    
    * Now visualizing cast count in session fragment
    
    * Now storing casts in DB
    
    * Remove stale TODO
    
    * Removed stale TODO
    
    * Using dedicated listener class
    
    * Use dedicated listener
    
    * Remove cast detection in step listener
    
    * Remove unused vars
    
    * Cleanup code
    
    * Add comment
    
    * Moved listener to own file
    
    * Switch on sensor type
    
    * Added countStep function
    
    * Using system step detector
    
    * Using step detector
    
    * Detect cast peak
    
    * Draft of cast detection
    
    * Database now stores casts per session
    
    * View casts on tag
    
    * Now visualizing cast count in session fragment
    
    * Now storing casts in DB
    
    * Remove stale TODO
    
    * Removed stale TODO
    
    * Using dedicated listener class
    
    * Use dedicated listener
    
    * Remove cast detection in step listener
    
    * Remove unused vars
    
    * Cleanup code
    
    * Add comment
    
    ---------
    
    Co-authored-by: LoreMolinari <lore.molinari.00@gmail.com>

---
+++ app/src/main/java/com/smartangler/smartangler/StepCounterListener.java
@@ -0,0 +1,129 @@
+package com.smartangler.smartangler;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+public class StepCounterListener implements SensorEventListener {
+
+    private long lastSensorUpdate = 0;
+    public static int accStepCounter = 0;
+    ArrayList<Integer> accSeries = new ArrayList<>();
+    ArrayList<String> timestampsSeries = new ArrayList<>();
+    private double accMag = 0;
+    private int lastAddedIndex = 1;
+    int stepThreshold = 6;
+
+    TextView stepCountsView;
+    TextView castsView;
+    CircularProgressIndicator progressBar;
+    private final SQLiteDatabase database;
+
+    private String timestamp;
+    private String day;
+    private String hour;
+
+    public StepCounterListener(TextView stepCountsView, CircularProgressIndicator progressBar, TextView castsView, SQLiteDatabase database) {
+        this.stepCountsView = stepCountsView;
+        this.database = database;
+        this.progressBar = progressBar;
+        this.castsView = castsView;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        long currentTimeInMilliSecond = System.currentTimeMillis();
+
+        long timeInMillis = currentTimeInMilliSecond + (sensorEvent.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
+
+        SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+        jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
+        String sensorEventDate = jdf.format(timeInMillis);
+
+        switch (sensorEvent.sensor.getType())
+        {
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                float x = sensorEvent.values[0];
+                float y = sensorEvent.values[1];
+                float z = sensorEvent.values[2];
+
+                if ((currentTimeInMilliSecond - lastSensorUpdate) > 1000) {
+                    lastSensorUpdate = currentTimeInMilliSecond;
+                    String sensorRawValues = "  x = " + x + "  y = " + y + "  z = " + z;
+                    Log.d("Acc. Event", "last sensor update at " + sensorEventDate + sensorRawValues);
+                }
+
+                accMag = Math.sqrt(x * x + y * y + z * z);
+
+                accSeries.add((int) accMag);
+
+                timestamp = sensorEventDate;
+                day = sensorEventDate.substring(0, 10);
+                hour = sensorEventDate.substring(11, 13);
+
+                Log.d("SensorEventTimestampInMilliSecond", timestamp);
+
+                timestampsSeries.add(timestamp);
+                peakDetection();
+                break;
+
+            case Sensor.TYPE_STEP_DETECTOR:
+                countStep(sensorEventDate);
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    private void peakDetection() {
+
+        int windowSize = 20;
+        int currentSize = accSeries.size();
+        if (currentSize - lastAddedIndex < windowSize) {
+            return;
+        }
+
+        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex, currentSize);
+        List<String> timePointList = timestampsSeries.subList(lastAddedIndex, currentSize);
+        lastAddedIndex = currentSize;
+
+        for (int i = 1; i < valuesInWindow.size() - 1; i++) {
+            int forwardSlope = valuesInWindow.get(i + 1) - valuesInWindow.get(i);
+            int downwardSlope = valuesInWindow.get(i) - valuesInWindow.get(i - 1);
+
+            if (forwardSlope < 0 && downwardSlope > 0 && valuesInWindow.get(i) > stepThreshold) { // Peak due to step
+                //  TODO: Used step detector only to count steps
+                countStep(timePointList.get(i));
+            }
+        }
+    }
+
+    private void countStep(String timestamp) {
+        accStepCounter += 1;
+        Log.d("ACC STEPS: ", String.valueOf(accStepCounter));
+        stepCountsView.setText(String.valueOf(accStepCounter));
+        progressBar.setProgress(accStepCounter);
+
+        ContentValues databaseEntry = new ContentValues();
+        databaseEntry.put(SmartAnglerOpenHelper.KEY_TIMESTAMP, timestamp);
+
+        databaseEntry.put(SmartAnglerOpenHelper.KEY_DAY, this.day);
+        databaseEntry.put(SmartAnglerOpenHelper.KEY_HOUR, this.hour);
+
+        database.insert(SmartAnglerOpenHelper.TABLE_NAME, null, databaseEntry);
+    }
+}
--- app/src/main/java/com/smartangler/smartangler/ui/fishing/FishingFragment.java
+++ app/src/main/java/com/smartangler/smartangler/ui/fishing/FishingFragment.java
@@ -3,7 +3,6 @@ package com.smartangler.smartangler.ui.fishing;
 import static android.app.Activity.RESULT_OK;
 
 import android.Manifest;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,14 +10,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,9 +28,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.smartangler.smartangler.CastDetectorListener;
 import com.smartangler.smartangler.R;
 import com.smartangler.smartangler.SmartAnglerOpenHelper;
 import com.smartangler.smartangler.SmartAnglerSessionHelper;
+import com.smartangler.smartangler.StepCounterListener;
 import com.smartangler.smartangler.databinding.FragmentFishingBinding;
 
 import java.io.ByteArrayOutputStream;
@@ -44,7 +41,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.TimeZone;
 
 public class FishingFragment extends Fragment {
 
@@ -61,11 +57,14 @@ public class FishingFragment extends Fragment {
     private int totalMinutes = 0;
 
     private TextView stepCountsView;
+    private TextView castsView;
     private CircularProgressIndicator progressBar;
     private MaterialButtonToggleGroup toggleButtonGroup;
+    private Sensor stepDetector;
     private Sensor accSensor;
     private SensorManager sensorManager;
     private StepCounterListener sensorListener;
+    private CastDetectorListener castDetectorListener;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -89,11 +88,14 @@ public class FishingFragment extends Fragment {
         progressBar.setMax(50);
         progressBar.setProgress(0);
 
+        castsView = root.findViewById(R.id.casts_text);
+
         try {
             sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
         } catch (NullPointerException e) {
             Toast.makeText(getContext(), R.string.acc_sensor_not_available, Toast.LENGTH_LONG).show();
         }
+        stepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR); // How about step counter instead of detector?
         accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         return binding.getRoot();
@@ -118,9 +120,17 @@ public class FishingFragment extends Fragment {
             SmartAnglerOpenHelper databaseOpenHelper = new SmartAnglerOpenHelper(this.getContext());
             SQLiteDatabase database = databaseOpenHelper.getWritableDatabase();
 
+            if (stepDetector != null) {
+                sensorListener = new StepCounterListener(stepCountsView, progressBar, castsView, database);
+                sensorManager.registerListener(sensorListener, stepDetector, SensorManager.SENSOR_DELAY_NORMAL);
+                Toast.makeText(getContext(), R.string.start_text, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getContext(), R.string.acc_sensor_not_available, Toast.LENGTH_LONG).show();
+            }
+
             if (accSensor != null) {
-                sensorListener = new com.smartangler.smartangler.ui.fishing.StepCounterListener(stepCountsView, progressBar, database);
-                sensorManager.registerListener(sensorListener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                castDetectorListener = new CastDetectorListener(castsView);
+                sensorManager.registerListener(castDetectorListener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
                 Toast.makeText(getContext(), R.string.start_text, Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(getContext(), R.string.acc_sensor_not_available, Toast.LENGTH_LONG).show();
@@ -138,7 +148,8 @@ public class FishingFragment extends Fragment {
                     "Unknown Location",
                     totalMinutes,
                     fish_caught,
-                    StepCounterListener.accStepCounter
+                    StepCounterListener.accStepCounter,
+                    CastDetectorListener.castsCounter
             );
             fish_caught = 0;
             isSessionActive = false;
@@ -246,104 +257,6 @@ public class FishingFragment extends Fragment {
             fishEntries.add(entry);
         }
         adapter.notifyDataSetChanged();
-    }
-}
-
-class StepCounterListener implements SensorEventListener {
-
-    private long lastSensorUpdate = 0;
-    public static int accStepCounter = 0;
-    ArrayList<Integer> accSeries = new ArrayList<>();
-    ArrayList<String> timestampsSeries = new ArrayList<>();
-    private double accMag = 0;
-    private int lastAddedIndex = 1;
-    int stepThreshold = 6;
-
-    TextView stepCountsView;
-    CircularProgressIndicator progressBar;
-    private final SQLiteDatabase database;
-
-    private String timestamp;
-    private String day;
-    private String hour;
-
-    public StepCounterListener(TextView stepCountsView, CircularProgressIndicator progressBar, SQLiteDatabase database) {
-        this.stepCountsView = stepCountsView;
-        this.database = database;
-        this.progressBar = progressBar;
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
-            long currentTimeInMilliSecond = System.currentTimeMillis();
-
-            long timeInMillis = currentTimeInMilliSecond + (sensorEvent.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
-
-            SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
-            jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
-            String sensorEventDate = jdf.format(timeInMillis);
-
-            if ((currentTimeInMilliSecond - lastSensorUpdate) > 1000) {
-                lastSensorUpdate = currentTimeInMilliSecond;
-                String sensorRawValues = "  x = " + x + "  y = " + y + "  z = " + z;
-                Log.d("Acc. Event", "last sensor update at " + sensorEventDate + sensorRawValues);
-            }
-
-            accMag = Math.sqrt(x * x + y * y + z * z);
-
-            accSeries.add((int) accMag);
-
-            timestamp = sensorEventDate;
-            day = sensorEventDate.substring(0, 10);
-            hour = sensorEventDate.substring(11, 13);
-
-            Log.d("SensorEventTimestampInMilliSecond", timestamp);
-
-            timestampsSeries.add(timestamp);
-            peakDetection();
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-    }
-
-    private void peakDetection() {
-
-        int windowSize = 20;
-        int currentSize = accSeries.size();
-        if (currentSize - lastAddedIndex < windowSize) {
-            return;
-        }
-
-        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex, currentSize);
-        List<String> timePointList = timestampsSeries.subList(lastAddedIndex, currentSize);
-        lastAddedIndex = currentSize;
-
-        for (int i = 1; i < valuesInWindow.size() - 1; i++) {
-            int forwardSlope = valuesInWindow.get(i + 1) - valuesInWindow.get(i);
-            int downwardSlope = valuesInWindow.get(i) - valuesInWindow.get(i - 1);
-
-            if (forwardSlope < 0 && downwardSlope > 0 && valuesInWindow.get(i) > stepThreshold) {
-                accStepCounter += 1;
-                Log.d("ACC STEPS: ", String.valueOf(accStepCounter));
-                stepCountsView.setText(String.valueOf(accStepCounter));
-                progressBar.setProgress(accStepCounter);
-
-                ContentValues databaseEntry = new ContentValues();
-                databaseEntry.put(SmartAnglerOpenHelper.KEY_TIMESTAMP, timePointList.get(i));
-
-                databaseEntry.put(SmartAnglerOpenHelper.KEY_DAY, this.day);
-                databaseEntry.put(SmartAnglerOpenHelper.KEY_HOUR, this.hour);
-
-                database.insert(SmartAnglerOpenHelper.TABLE_NAME, null, databaseEntry);
-            }
-        }
     }
 }
 
