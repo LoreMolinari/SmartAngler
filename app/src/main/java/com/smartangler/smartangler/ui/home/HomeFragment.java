175fd61fc4196963525c26834a7f31af3613dc97
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-12-03 13:33:13 +0100

    Accessing location

--- app/src/main/java/com/smartangler/smartangler/ui/home/HomeFragment.java
+++ app/src/main/java/com/smartangler/smartangler/ui/home/HomeFragment.java
@@ -1,17 +1,33 @@
 package com.smartangler.smartangler.ui.home;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.smartangler.smartangler.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
 
     private FragmentHomeBinding binding;
 
@@ -24,6 +40,22 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Check location permission and request if not granted
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+            Log.d("Location service", "Location access granted");
+        } else {
+            Log.d("Location service", "Location access already granted");
+        }
     }
 
     @Override
