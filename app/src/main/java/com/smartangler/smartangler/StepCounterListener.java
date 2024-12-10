811138217739954161fb554220597a08af0c3605
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-12-10 16:43:51 +0100

    Progress bar is for casting only (#35)
    * Don't update progress bar on step
    
    * Remove unused vars

--- app/src/main/java/com/smartangler/smartangler/StepCounterListener.java
+++ app/src/main/java/com/smartangler/smartangler/StepCounterListener.java
@@ -1,28 +1,19 @@
 package com.smartangler.smartangler;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.material.progressindicator.CircularProgressIndicator;
-
 public class StepCounterListener implements SensorEventListener {
 
     private long initialStepCount = -1;
     public static int stepCount = 0;
     private TextView stepCountsView;
-    private TextView counterPB;
-    private CircularProgressIndicator progressBar;
-    private SQLiteDatabase database;
 
-    public StepCounterListener(TextView stepCountsView, TextView counterPB, CircularProgressIndicator progressBar, TextView castsView, SQLiteDatabase database) {
+    public StepCounterListener(TextView stepCountsView) {
         this.stepCountsView = stepCountsView;
-        this.counterPB = counterPB;
-        this.progressBar = progressBar;
-        this.database = database;
     }
 
     @Override
@@ -47,7 +38,5 @@ public class StepCounterListener implements SensorEventListener {
 
     private void updateUI() {
         stepCountsView.setText("Steps: " + stepCount);
-        counterPB.setText(String.valueOf(stepCount));
-        progressBar.setProgress(stepCount);
     }
 }
